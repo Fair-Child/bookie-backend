2f282b7c8833335e398b2733e3a9bcdcb0c74ade
HF <brentliang94@gmail.com>
2023-08-23 22:05:43 -0400

    added review endpoint service and controller

--- bookie/src/main/java/com/fairchild/bookie/dao/ReviewRepository.java
+++ bookie/src/main/java/com/fairchild/bookie/dao/ReviewRepository.java
@@ -10,4 +10,6 @@ public interface ReviewRepository extends JpaRepository<Review, Long> {
 
     Page<Review> findByBookId(@RequestParam("book_id") long bookId, Pageable pageable);
 
+    Review findByUserEmailAndBookId(String userEmail, Long bookId);
+
 }
